c16cb0e915187785ffdefce535c3be930c4846fc
Timisalam <timisalam7@gmail.com>
2023-11-23 12:57:37 +0000

    Update app.js
    updtaed aniumation to go when page is loaded

--- app.js
+++ app.js
@@ -4,12 +4,10 @@ const openMenu = document.querySelector('.openMenu');
 const menu_items = document.querySelectorAll('nav .mainMenu li a');
 const background = document.querySelector('.background-img-container');
 const title = document.querySelector('.name');
-const phoneBackground = document.querySelector('.mainImgDiv');
 
 openMenu.addEventListener('click',show);
 closeMenu.addEventListener('click',close);
-background.addEventListener('mouseover',showName);
-phoneBackground.addEventListener('mouseover',showName)
+document.addEventListener('DOMContentLoaded',showName)
 // close menu when you click on a menu item 
 menu_items.forEach(item => {
     item.addEventListener('click',function(){
