a1b016c89a7965b5462ca7700584f41e64727128
Timisalam <timisalam7@gmail.com>
2024-03-03 15:30:54 +0000

    gallery and sport page done
    gallery and sport page done for phone could potentially change some photos in sports and need to resize photos for larger screens

--- app.js
+++ app.js
@@ -4,6 +4,10 @@ const openMenu = document.querySelector('.openMenu');
 const menu_items = document.querySelectorAll('nav .mainMenu li a');
 const background = document.querySelector('.background-img-container');
 const title = document.querySelector('.name');
+const portrait = document.querySelector('.portrait')
+const portraitTitle = document.querySelector('.portrait-name')
+const landscapes = document.querySelector('.landscapes')
+const landscapesTitle = document.querySelector('.landscapes-name')
 
 openMenu.addEventListener('click',show);
 closeMenu.addEventListener('click',close);
@@ -31,3 +35,15 @@ function showName(){
         title.style.top = '50%';
     }, 200);
 }
+portrait.addEventListener('mouseover',e => {
+    setTimeout(function () {
+        portraitTitle.style.transition = 'top 1s ease'; // Set the transition property
+        portraitTitle.style.top = '150%';
+    }, 100);
+})
+landscapes.addEventListener('mouseover',e => {
+    setTimeout(function () {
+        landscapesTitle.style.transition = 'top 1s ease'; // Set the transition property
+        landscapesTitle.style.top = '250%';
+    }, 100);
+})
